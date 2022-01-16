f705adec86ba42ce1e673d183cacd4ae011dd9aa
omgpiu <dyba@list.ru>
2022-01-16 21:06:21 +0300

    if,else if,switch

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,24 @@
 "use strict";
-// const numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', '');
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   private: false
-// };
-// const seenMovieQuestion = prompt ("Один из последних просмотренных фильмов?","");
-// const movieRate = prompt ("На сколько оцените его?","");
-// const seenMovieQuestionSecond = prompt ("Один из последних просмотренных фильмов?","");
-// const movieRateSecond = prompt ("На сколько оцените его?","");
-// personalMovieDB.movies[seenMovieQuestion] = movieRate;
-// personalMovieDB.movies[seenMovieQuestionSecond] = movieRateSecond;
-// console.log(personalMovieDB);
+const numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+const seenMovieQuestion = prompt ("Один из последних просмотренных фильмов?","");
+const movieRate = prompt ("На сколько оцените его?","");
+const seenMovieQuestionSecond = prompt ("Один из последних просмотренных фильмов?","");
+const movieRateSecond = prompt ("На сколько оцените его?","");
+personalMovieDB.movies[seenMovieQuestion] = movieRate;
+personalMovieDB.movies[seenMovieQuestionSecond] = movieRateSecond;
+console.log(personalMovieDB);
 if (1) {
   console.log("OK!");
+
+
+  
 } else {
   console.log("Error");
 }
